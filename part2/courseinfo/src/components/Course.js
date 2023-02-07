6f282236ab0324b2a1cda6ad9f7c2c2a2c5a2a41
git@tokyuez.net <github@tokyuez.net>
2023-02-07 15:51:11 +0100

    ex 2.2

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,2 +1,48 @@
-const Course = () => {};
+const Part = (part) => (
+  <>
+    <li>
+      {part.name} {part.exercises}
+    </li>
+  </>
+);
+
+const Content = ({ parts }) => (
+  <>
+    <ul>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </ul>
+  </>
+);
+
+const Header = (props) => {
+  // console.log("props", props);
+  return (
+    <>
+      <h1>{props.name}</h1>
+    </>
+  );
+};
+const Total = ({ parts }) => {
+  // console.log("parts", parts);
+  // console.log("part1", parts[0]);
+  // console.log("part1 ex", parts[0].exercises);
+  let sum = parts.reduce((a, v) => (a = a + v.exercises), 0);
+  return (
+    <>
+      <p>total of {sum} exercises</p>
+    </>
+  );
+};
+const Course = ({ course }) => {
+  // console.log(course);
+  return (
+    <>
+      <Header name={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
+    </>
+  );
+};
 export default Course;
